4c4e5e0024b068946072aa821490890da74e647e
jmbrando <jmbrando@ualberta.ca>
2021-02-16 17:58:56 -0700

    scripts added for all grids, css adjusted

--- js/main.js
+++ js/main.js
@@ -20,9 +20,32 @@ menuExit.addEventListener('click', function(){
 });
 
 
+// graphic scripts
+
+var graphicA = document.querySelectorAll('.graphic-open').forEach(graphicOpen => {
+
+  graphicOpen.addEventListener('click', function(){
+
+    $(this).siblings().toggleClass('show-details');
+    $(this).parents('.graphic-item').toggleClass('add-bg');
+
+  });
+});
+
+var graphicB = document.querySelectorAll('.graphic-close').forEach(graphicClose => {
+
+  graphicClose.addEventListener('click', function(){
+
+    $(this).parents('.closing-area').siblings('.graphic-open').siblings().toggleClass('show-details');
+    $(this).parents('.graphic-item').toggleClass('add-bg');
+
+  });
+});
+
+
 // web scripts
 
-var webA = document.querySelectorAll('.item-open').forEach(webOpen => {
+var webA = document.querySelectorAll('.web-open').forEach(webOpen => {
 
   webOpen.addEventListener('click', function(){
 
@@ -30,45 +53,14 @@ var webA = document.querySelectorAll('.item-open').forEach(webOpen => {
     $(this).parents('.web-item').toggleClass('add-bg');
 
   });
-
 });
 
-var webB = document.querySelectorAll('.close-btn').forEach(webClose => {
+var webB = document.querySelectorAll('.web-close').forEach(webClose => {
 
   webClose.addEventListener('click', function(){
 
-    $(this).parents('.closing-area').siblings('.item-open').siblings().toggleClass('show-details');
+    $(this).parents('.closing-area').siblings('.web-open').siblings().toggleClass('show-details');
     $(this).parents('.web-item').toggleClass('add-bg');
 
   });
-
 });
-
-// var web1b = document.querySelector('#web1 .close-btn');
-//
-// web1b.addEventListener('click', function(){
-//
-//   $('#web1 .item-open').siblings().toggleClass('show-details');
-//   $('#web1 .close-btn').parents('section.web-item').toggleClass('add-bg');
-//
-// });
-//
-// // item 2
-//
-// var web2a = document.querySelector('#web2 .item-open');
-//
-// web2a.addEventListener('click', function(){
-//
-//   $('#web2 .item-open').siblings().toggleClass('show-details');
-//   $('#web2').toggleClass('add-bg');
-//
-// });
-//
-// var web2b = document.querySelector('#web2 .close-btn');
-//
-// web2b.addEventListener('click', function(){
-//
-//   $('#web2 .item-open').siblings().toggleClass('show-details');
-//   $('#web2 .close-btn').parents('section.web-item').toggleClass('add-bg');
-//
-// });
